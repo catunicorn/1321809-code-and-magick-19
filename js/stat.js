9fa777eeffb32459e21b7a51c479f10804cecc70
Tanya <alfamuse@gmail.com>
2020-01-28 23:49:16 +0300

    Бекап измененных файлов3

--- js/stat.js
+++ js/stat.js
@@ -5,7 +5,6 @@ var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
 var GAP = 10;
-var FONT_GAP = 15;
 var TEXT_WIDTH = 20;
 var BAR_Y = 80;
 var BAR_WIDTH = 40;
@@ -17,7 +16,7 @@ var renderCloud = function (ctx, x, y, color) {
 };
 
 var fetchColor = function (name) {
-  return name.toLowerCase() == 'вы' ? 'rgba(255, 0, 0, 1)' : 'hsl(240, ' + Math.round(Math.random(100) * 100) + '%, 50%)';
+  return name.toLowerCase() === 'вы' ? 'rgba(255, 0, 0, 1)' : 'hsl(240, ' + Math.round(Math.random(100) * 100) + '%, 50%)';
 };
 
 var fetchHeight = function (currentHeight, maxHeight) {
@@ -33,7 +32,10 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.fillText('Ура вы победили!', 120, 40);
   ctx.fillText('Список результатов:', 120, 55);
 
-  var maxHeight = Math.max(...times);
+  var maxHeight = times.reduce(function (a, b) {
+    return Math.max(a, b);
+  });
+
   for (var i = 0; i < names.length; i++) {
     var cordX = CLOUD_X + BAR_WIDTH * (i + 1) + 50 * i;
     var height = fetchHeight(times[i], maxHeight);
